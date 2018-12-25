7ce0d8c9dd405b13a6bd984921f0b7e0bd978ebd
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-12-25 21:51:51 +0200

    add 'swirl' background

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Shift from './components/Shift'
+import Swirl from './components/Swirl'
 import Pipeline from './components/Pipeline'
 import CanvasBackground from './components/CanvasBackground'
 
 export {
 	Shift,
+	Swirl,
 	Pipeline,
 	CanvasBackground
 };
