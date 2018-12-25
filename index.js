d0c951c8e399afdab129adf76d73826dc0b51f3c
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-12-25 23:13:45 +0200

    fix modules' export, v0.2.1

---
+++ index.js
@@ -0,0 +1,15 @@
+import Shift from './src/components/Shift'
+import Swirl from './src/components/Swirl'
+import Aurora from './src/components/Aurora'
+import Coalesce from './src/components/Coalesce'
+import Pipeline from './src/components/Pipeline'
+import CanvasBackground from './src/components/CanvasBackground'
+
+export default {
+	Shift,
+	Swirl,
+	Aurora,
+	Coalesce,
+	Pipeline,
+	CanvasBackground
+};
