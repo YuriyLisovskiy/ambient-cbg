2ee4feb2a8f492157fee66740da9d9726344d375
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-12-26 16:55:38 +0200

    Updates

---
+++ precache-manifest.2b38b350f3640f534b79987f1086707c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ambient-cbg/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5e79dc8775b8ebf40544",
+    "url": "/ambient-cbg/static/js/runtime~main.5e79dc87.js"
+  },
+  {
+    "revision": "ded54b1866acc0d912f4",
+    "url": "/ambient-cbg/static/js/main.ded54b18.chunk.js"
+  },
+  {
+    "revision": "528cfe48adfe5353d5a0",
+    "url": "/ambient-cbg/static/js/1.528cfe48.chunk.js"
+  },
+  {
+    "revision": "ded54b1866acc0d912f4",
+    "url": "/ambient-cbg/static/css/main.10ea4d12.chunk.css"
+  },
+  {
+    "revision": "b716810dedd64b612f12862e3ac37da5",
+    "url": "/ambient-cbg/index.html"
+  }
+];
