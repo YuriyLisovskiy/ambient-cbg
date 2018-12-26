d5397eb162ac2ea4f6a5a6324349fc23de9c64f5
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-12-26 14:35:30 +0200

    add example project

---
+++ docs/precache-manifest.767ffa842c6f6a2d33c9f343602f1a3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6121a5e500b8327a0d22",
+    "url": "/static/js/main.6121a5e5.chunk.js"
+  },
+  {
+    "revision": "528cfe48adfe5353d5a0",
+    "url": "/static/js/1.528cfe48.chunk.js"
+  },
+  {
+    "revision": "6121a5e500b8327a0d22",
+    "url": "/static/css/main.10ea4d12.chunk.css"
+  },
+  {
+    "revision": "e9fc839b11ea1636d95e167ad171b276",
+    "url": "/index.html"
+  }
+];
